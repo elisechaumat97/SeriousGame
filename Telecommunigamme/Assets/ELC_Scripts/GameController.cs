2aafdccf2362671b062894f9c8434affc68e17b8
EliseChaumat97 <elise.chaumat@supelec.fr>
2019-04-30 14:03:41 +0200

    first commit
    Fisrt Commit

---
+++ Telecommunigamme/Assets/ELC_Scripts/GameController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public float speed;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(speed * Vector3.right);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(speed * Vector3.left);
+        }
+    }
+}
---
+++ Telecommunigamme/Assets/ELC_Scripts/Hiro.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hiro : MonoBehaviour
+{
+    public GameObject Alaïa;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(Alaïa.transform.position.x - 1, Alaïa.transform.position.y, Alaïa.transform.position.z);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(transform.position.x - Alaïa.transform.position.x) >= 2) {
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                transform.position = new Vector3(Alaïa.transform.position.x - 2, Alaïa.transform.position.y, Alaïa.transform.position.z);
+                transform.position = new Vector3(Alaïa.transform.position.x - 1.8f, Alaïa.transform.position.y, Alaïa.transform.position.z);
+            }
+            
+            if (Input.GetKey(KeyCode.LeftArrow))
+            {
+              
+                transform.position = new Vector3(Alaïa.transform.position.x + 2, Alaïa.transform.position.y, Alaïa.transform.position.z);
+                
+                transform.position = new Vector3(Alaïa.transform.position.x + 1.8f, Alaïa.transform.position.y, Alaïa.transform.position.z);
+            }
+
+        }
+    }
+}
