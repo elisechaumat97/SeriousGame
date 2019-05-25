21ed0c302429110520c3a87147280310a27cdf1f
EliseChaumat97 <elise.chaumat@supelec.fr>
2019-05-25 21:52:17 +0200

    none

--- Telecommunigamme/Assets/ELC_Scripts/GameController.cs
+++ Telecommunigamme/Assets/ELC_Scripts/GameController.cs
@@ -7,30 +7,21 @@ public class GameController : MonoBehaviour
     public float speed=100;
     Vector3 target;
     bool move = false;
-    public static GameController instance = null;
 
-    void Start()
-    {
-
-        if (instance == null)
-        {
-            instance = this;
-        }
-    }
+   
 
         // Update is called once per frame
         void Update()
        
     {
-        transform.rotation = new Quaternion(0,0,0,0);
+        //transform.rotation = new Quaternion(0,0,0,0);
         
 
         if (Input.GetMouseButtonDown(0))
         {
             move = true;
             target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            target.y = transform.position.y;
-            target.z = transform.position.z;
+           
         }
         if(move == true)
             {
@@ -47,19 +38,7 @@ public class GameController : MonoBehaviour
 
     }
 
-    public void MoveObject(Vector3 targetObject)
-    {
-        move = true;
-        if (move == true)
-        {
-            transform.position = Vector3.MoveTowards(transform.position, targetObject, speed);
-            Debug.Log(targetObject);
-        }
-        if (targetObject == transform.position)
-        {
-            move = false;
-        }
-    }
+    
  
    
     
