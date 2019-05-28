a6efc0f4cc1e4b0e2304b653c24ac17ca438cb59
PSterlin <philibert.sterlin@gmail.com>
2019-05-28 17:37:51 +0200

    Ajout de perspective, tentative de parallaxe (je sais pas encore si ça marche)

---
+++ Telecommunigamme/Assets/Scripts/PHS_Scripts/Perspective.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Perspective : MonoBehaviour
+{
+    public float furthestY;     //Pour une perspective linéaire
+    public float closestY;
+    public float furthestScale; 
+    public float closestScale;
+    private Vector3 modifiedScale;
+    // Update is called once per frame
+    private void Update()
+    {
+        modifiedScale = new Vector3(furthestScale + ((transform.position.y - furthestY) / (closestY - furthestY))*(closestScale-furthestScale), furthestScale + ((transform.position.y - furthestY) / (closestY - furthestY)) * (closestScale - furthestScale), 0);
+        transform.localScale = modifiedScale;
+    }
+}
