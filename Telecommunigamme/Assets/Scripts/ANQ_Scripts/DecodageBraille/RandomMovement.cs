3459b8a739d29ebfc323b4cca3b6b26d55b477d1
AntoineQuesnel <AntoineQuesnel@users.noreply.github.com>
2019-05-29 11:33:08 +0200

    Merge branch 'Menu' of https://github.com/elisechaumat97/SeriousGame
    # Conflicts:
    #	Telecommunigamme/Assets/Characters.meta
    #	Telecommunigamme/Assets/Cinemachine/CinemachineExamples.unitypackage.meta
    #	Telecommunigamme/Assets/PHS_Scenes/Level0_1_bis.unity.meta
    #	Telecommunigamme/Assets/_Scenes/ANQ_Scenes/Scene Codage Morse.unity.meta

---
+++ Telecommunigamme/Assets/Scripts/ANQ_Scripts/DecodageBraille/RandomMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour
+{
+    public float maxSpeed = 5f;
+    public float maxTimeChangeDirection;
+    private Vector2 movement;
+    private float timeLeft;
+    public Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        timeLeft = Random.Range(0f, 1f);
+    }
+
+    void Update()
+    {
+        if (timeLeft <= 0)
+        {
+            movement = new Vector2(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
+            timeLeft = Random.Range(0f, maxTimeChangeDirection);
+        }
+        else
+        {
+            timeLeft -= 0.1f;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.AddForce(movement * maxSpeed);
+    }
+}
