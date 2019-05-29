59bfc562b7513eec04b1c9956d5fc05f4f596f81
AntoineQuesnel <AntoineQuesnel@users.noreply.github.com>
2019-05-30 00:49:59 +0200

    Premiere partie de l'enigme 1 fonctionnelle

---
+++ Telecommunigamme/Assets/Scripts/ANQ_Scripts/DecodageBraille/ANQ_Catch.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANQ_Catch : MonoBehaviour
+{
+    public GameObject butterfly;
+    public int butterflyNumber;
+
+    void Start()
+    {
+        for (int i = 0; i <= butterflyNumber; i++)
+        {
+            Instantiate(butterfly, new Vector3(Random.Range(-2f, 2f), Random.Range(-2f, 2f), 0), Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            foreach (Collider2D col in colliders)
+            {
+                col.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private List<Collider2D> colliders = new List<Collider2D>();
+    public List<Collider2D> GetColliders() { return colliders; }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (!colliders.Contains(other)) { colliders.Add(other); }
+    }
+
+    public void OnTriggerExit2D(Collider2D other)
+    {
+        colliders.Remove(other);
+    }
+
+}
