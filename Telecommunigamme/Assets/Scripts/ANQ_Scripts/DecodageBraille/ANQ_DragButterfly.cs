041fd3cff8f4c328d10b507a6314b9c5b141293b
AntoineQuesnel <AntoineQuesnel@users.noreply.github.com>
2019-06-07 18:18:01 +0200

    no message

---
+++ Telecommunigamme/Assets/Scripts/ANQ_Scripts/DecodageBraille/ANQ_DragButterfly.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANQ_DragButterfly : MonoBehaviour
+{
+    private float startPosX;
+    private float startPosY;
+    private bool isBeingHeld = false;
+
+
+    void Update()
+    {
+        if (isBeingHeld)
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            this.gameObject.transform.localPosition = new Vector3(mousePos.x - startPosX, 0, 0);
+        }
+    }
+
+    private void OnMouseDown()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePos;
+            mousePos = Input.mousePosition;
+            mousePos = Camera.main.ScreenToWorldPoint(mousePos);
+
+            startPosX = mousePos.x - this.transform.localPosition.x;
+            startPosY = mousePos.y - this.transform.localPosition.y;
+
+        }
+
+
+        isBeingHeld = true;
+    }
+
+    private void OnMouseUp()
+    {
+        isBeingHeld = false;
+    }
+}
---
+++ Telecommunigamme/Assets/Scripts/ANQ_Scripts/DecodageBraille/ANQ_GenerateBigButterfly.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ANQ_GenerateBigButterfly : MonoBehaviour
+{
+    public GameObject bigButterfly;
+    public int bigButterflyNumber;
+
+    void Start()
+    {
+        for (int i = 0; i <= bigButterflyNumber; i++)
+        {
+            Instantiate(bigButterfly, new Vector3(2*i-bigButterflyNumber, 0, 0), Quaternion.identity) as ;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
