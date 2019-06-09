715b2e5c000157774746e0b2d0bb0a7961a9c61d
EliseChaumat97 <elise.chaumat@supelec.fr>
2019-06-09 15:47:18 +0200

    tilemap
    ajout d'une grille pour ne pas cliquer de partout

--- Telecommunigamme/Assets/Scripts/ELC_Scripts/clickmouse.cs
+++ Telecommunigamme/Assets/Scripts/ELC_Scripts/clickmouse.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class clickmouse : MonoBehaviour
 {
     Vector3 mouspos;
+    public Tilemap grid;
     public GameObject player;
     // Start is called before the first frame update
     void Start()
@@ -19,7 +21,13 @@ public class clickmouse : MonoBehaviour
         {
             mouspos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, Input.mousePosition.z);
             mouspos = Camera.main.ScreenToWorldPoint(mouspos);
-            this.transform.position = new Vector3(mouspos.x, mouspos.y, -1);
+            GridLayout gridLayout = grid.GetComponentInParent<GridLayout>();
+            Vector3Int cellPosition = gridLayout.WorldToCell(mouspos);
+
+            if (!grid.HasTile(cellPosition))
+            {
+                this.transform.position = new Vector3(mouspos.x, mouspos.y, -1);
+            }
         }
     }
 }
